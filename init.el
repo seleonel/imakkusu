85a3e317f8911090392e4ae0ba3828c0ba00891d
seleonel <leon_bellini@outlook.com>
2020-11-24 03:46:05 -0300

    Adicionados mais configs e git grep como keybind

--- init.el
+++ init.el
@@ -21,26 +21,3 @@
 (straight-use-package 'gnus)
 ;; org configs load
 (org-babel-load-file (expand-file-name (concat (getenv "XDG_CONFIG_HOME") "/emacs/organizator.org")))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(browse-url-browser-function 'browse-url-chromium)
- '(custom-safe-themes
-   '("8885761700542f5d0ea63436874bf3f9e279211707d4b1ca9ed6f53522f21934" default))
- '(linum-format " %5i ")
- '(org-format-latex-options
-   '(:foreground default :background default :scale 1.6 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
- '(org-src-window-setup 'split-window-below)
- '(pyvenv-mode t)
- '(reb-re-syntax 'string)
- '(safe-local-variable-values '((org-download-image-dir . \.\./\.\./imgs/)))
- '(send-mail-function 'mailclient-send-it)
- '(shell-escape-mode "-shell-escape"))
-
-
-
-
