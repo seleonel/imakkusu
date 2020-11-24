57b1c3a21382b72ce5ff8ab277cb0a86a25ba065
seleonel <leon_bellini@outlook.com>
2020-11-24 01:35:45 -0300

    Testando defer

--- init.el
+++ init.el
@@ -31,7 +31,6 @@
  '(custom-safe-themes
    '("8885761700542f5d0ea63436874bf3f9e279211707d4b1ca9ed6f53522f21934" default))
  '(linum-format " %5i ")
- '(org-agenda-files '("~/.config/emacs/organizator.org"))
  '(org-format-latex-options
    '(:foreground default :background default :scale 1.6 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
