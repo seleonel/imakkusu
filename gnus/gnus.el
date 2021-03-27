300549b3f42967de2781b7ac8ca48c8dccfa23e2
seleonel <leon_bellini@outlook.com>
2021-03-27 16:04:55 -0300

    Melhorado integração com lsp-mode

--- gnus/gnus.el
+++ gnus/gnus.el
@@ -4,7 +4,7 @@
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 (gnus-desktop-notify-mode)
 (gnus-demon-add-scanmail)
-(gnus-demon-add-handler 'gnus-demon-scan-news 10 t)
+(gnus-demon-add-handler 'gnus-demon-scan-news 5 5)
 ;; (gnus-demon-close-connections nil 60)
 (defadvice gnus-demon-scan-news (around gnus-demon-timeout activate)
   "Timeout for Gnus."
